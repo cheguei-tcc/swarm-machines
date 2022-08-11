f8950068bb08cdb35bc5434c22a4ede76f40c57f
iamseki <chrisyuri_19@hotmail.com>
2022-08-10 23:47:31 -0300

    separate nginx subdomains

--- aws/dns.tf
+++ aws/dns.tf
@@ -43,6 +43,21 @@ resource "aws_route53_record" "deployments_subdomains" {
   }
 }
 
+## createa a subodmain containers.cheguei.app to containers_cheguei ec2 instance
+resource "aws_route53_record" "deployments_subdomains_metrics" {
+  allow_overwrite = true
+  zone_id         = data.aws_route53_zone.base_domain.id
+  name            = "swarmpit.cheguei.app"
+  type            = "A"
+
+  alias {
+    name                   = aws_alb.swarm_cheguei_lb.dns_name
+    zone_id                = aws_alb.swarm_cheguei_lb.zone_id
+    evaluate_target_health = false
+  }
+}
+
+
 # This tells terraform to cause the route53 validation to happen
 resource "aws_acm_certificate_validation" "swarm_cert" {
   certificate_arn         = aws_acm_certificate.cheguei_domain_cert.arn
